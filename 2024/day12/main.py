631d510c009ed284c19262e4bd311c2dc949d427
Vitalik <frantsv2004@gmail.com>
2024-12-13 00:05:03 +0100

    Are You Winning Son? (easier to read)

--- 2024/day12/main.py
+++ 2024/day12/main.py
@@ -1,7 +1,6 @@
 import argparse
 import sys
 import os
-from collections import defaultdict
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from lib import lib
@@ -35,26 +34,24 @@ def silver(lines: list[str]) -> int:
     '''Solves the silver problem'''
     def dfs(data, row, col, char, seen):
         if row < 0 or row >= len(data) or col < 0 or col >= len(data[0]):
-            return (0, 0)
+            return (seen, 0)
         if data[row][col] != char or (row, col) in seen:
-            return (0, 0)
-        pers = 0
+            return (seen, 0)
+        borders = 0
         if row == 0 or data[row-1][col] != char:
-            pers += 1
+            borders += 1
         if row == len(data)-1 or data[row+1][col] != char:
-            pers += 1
+            borders += 1
         if col == 0 or data[row][col-1] != char:
-            pers += 1
+            borders += 1
         if col == len(data[0])-1 or data[row][col+1] != char:
-            pers += 1
+            borders += 1
         seen.add((row, col))
-        s = 1
         for r, c in lib.DIRS:
-            n = dfs(data, row+r, col+c, char, seen)
-            s += n[0]
-            pers += n[1]
+            seen, bdrs = dfs(data, row+r, col+c, char, seen)
+            borders += bdrs
         data[row][col] = '.'
-        return s, pers
+        return seen, borders
 
     data = parse_data(lines)
     ans = 0
@@ -62,8 +59,8 @@ def silver(lines: list[str]) -> int:
     for row, line in enumerate(data):
         for col, el in enumerate(line):
             if el != '.':
-                s, p = dfs(data, row, col, el, set())
-                ans += s * p
+                square, perimetr = dfs(data, row, col, el, set())
+                ans += len(square) * perimetr
 
     return ans
 
@@ -72,32 +69,25 @@ def gold(lines: list[str]) -> int:
     '''Solves the gold problem'''
     def dfs(data, row, col, char, seen):
         if row < 0 or row >= len(data) or col < 0 or col >= len(data[0]):
-            return 0, seen
+            return seen
         if data[row][col] != char or (row, col) in seen:
-            return 0, seen
+            return seen
         seen.add((row, col))
-        s = 1
         for r, c in lib.DIRS:
-            n = dfs(data, row+r, col+c, char, seen)
-            s += n[0]
-            seen |= n[1]
+            seen = dfs(data, row+r, col+c, char, seen)
         data[row][col] = '.'
-        return s, seen
+        return seen
 
     def count_triples(seen):
-        r_min, r_max, c_min, c_max = float('inf'), float('-inf'), float('inf'), float('-inf')
-        for r, c in seen:
-            r_min = min(r_min, r)
-            r_max = max(r_max, r)
-            c_min = min(c_min, c)
-            c_max = max(c_max, c)
+        r_min = min(r for r, _ in seen)
+        r_max = max(r for r, _ in seen)
+        c_min = min(c for _, c in seen)
+        c_max = max(c for _, c in seen)
         res = 0
+
         for r in range(r_min-1, r_max+1):
             for c in range(c_min-1, c_max+1):
-                count = 0
-                for rr, cc in ((0, 0), (0, 1), (1, 0), (1, 1)):
-                    if (r+rr, c+cc) in seen:
-                        count += 1
+                count = len([1 for rr, cc in ((0, 0), (0, 1), (1, 0), (1, 1)) if (r+rr, c+cc) in seen])
                 if count == 1 or count == 3:
                     res += 1
                 if count == 2:
@@ -109,16 +99,11 @@ def gold(lines: list[str]) -> int:
     data = parse_data(lines)
     ans = 0
 
-    for line in data:
-        print("".join(line))
-
     for row, line in enumerate(data):
         for col, el in enumerate(line):
             if el != '.':
-                s, seen = dfs(data, row, col, el, set())
-                p = count_triples(seen)
-                print(f"({row}, {col}) {el}: {s} * {p}")
-                ans += s * p
+                seen = dfs(data, row, col, el, set())
+                ans += len(seen) * count_triples(seen)
 
     return ans
 
