b78daf1c1e5b916689360f44bdcbd5d008959a67
Vitalij Franc <vitalij.franc@acl.at>
2025-12-02 06:51:28 +0100

    day 2 year 2025

---
+++ 2025/day2/main.py
@@ -0,0 +1,63 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from lib import lib
+
+INPUT_FILE = os.path.join(os.path.dirname(__file__), 'input.txt')
+TEST_FILE = os.path.join(os.path.dirname(__file__), 'test.txt')
+
+
+def parse_data(content: str):
+    '''Parses the data'''
+    return map(lambda x: x.split('-'), content.splitlines()[0].split(','))
+
+
+def silver(content: str, debug: bool = False):
+    '''Solves the silver problem'''
+    data = parse_data(content)
+    res = 0
+
+    for start, end in data:
+        for nbr in range(int(start), int(end)+1):
+            s = str(nbr)
+            if len(s) % 2 == 0:
+                if s[:len(s) // 2] == s[len(s) // 2:]:
+                    res += int(s)
+
+    return res
+
+
+def gold(content: str, debug: bool = False):
+    '''Solves the gold problem'''
+    data = parse_data(content)
+    res = 0
+
+    for start, end in data:
+        for nbr in range(int(start), int(end)+1):
+            s = str(nbr)
+            for i in range(2, len(s)+1):
+                if len(s) % i == 0:
+                    if s.count(s[:len(s) // i]) == i:
+                        res += int(s)
+                        break
+
+    return res
+
+
+def main():
+    '''Parses the input and solves the two problems'''
+    options = lib.parse_args()
+
+    content = lib.read_file(TEST_FILE)
+    print(f"Silver test: {silver(content, debug=True)}")
+    print(f"Gold test:   {gold(content, debug=True)}")
+
+    if not options.debug:
+        content = lib.read_file(INPUT_FILE)
+        print(f"Silver:      {silver(content)}")
+        print(f"Gold:        {gold(content)}")
+
+
+if __name__ == "__main__":
+    main()
